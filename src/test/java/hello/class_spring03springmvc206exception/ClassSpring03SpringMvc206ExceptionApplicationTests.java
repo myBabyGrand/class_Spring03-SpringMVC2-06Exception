9ca895b60a133125cde2891a6d0a1ada94eb52c9
mybabygrand <amuro_ray@kakao.com>
2022-03-01 21:51:44 +0900

    init

---
+++ src/test/java/hello/class_spring03springmvc206exception/ClassSpring03SpringMvc206ExceptionApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.class_spring03springmvc206exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassSpring03SpringMvc206ExceptionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
