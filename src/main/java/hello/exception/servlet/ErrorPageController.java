f77f205104a854d1919bf44f63deac470ad632f9
mybabygrand <amuro_ray@kakao.com>
2022-03-04 23:00:12 +0900

    Section 10. Error Handling, API : start  - API error controller.  - Spring boot basic error handling.

--- src/main/java/hello/exception/servlet/ErrorPageController.java
+++ src/main/java/hello/exception/servlet/ErrorPageController.java
@@ -1,11 +1,17 @@
 package hello.exception.servlet;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -30,6 +36,19 @@ public class ErrorPageController {
         log.info("error page 500");
         printErrorInfo(request, response);
         return "error-page/500";
+    }
+
+    @RequestMapping(value = "/error-page/500", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Map<String, Object>> errorPage500API(
+            HttpServletRequest request, HttpServletResponse response){
+        log.info("API error Page 500");
+
+        Map<String, Object> result = new HashMap<>();
+        Exception ex = (Exception) request.getAttribute(ERROR_EXCEPTION);
+        result.put("status", request.getAttribute(ERROR_STATUS_CODE));
+        result.put("message", ex.getMessage());
+        Integer httpStatusCode = (Integer) request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        return new ResponseEntity(result, HttpStatus.valueOf(httpStatusCode));
     }
 
     private void printErrorInfo(HttpServletRequest request, HttpServletResponse response){
