acdfaf357288351db928b808287b910364584dde
mybabygrand <amuro_ray@kakao.com>
2022-03-07 23:08:14 +0900

    Section 10. Error Handling, API  - HandlerException Resolver

--- src/main/java/hello/exception/api/ApiExceptionController.java
+++ src/main/java/hello/exception/api/ApiExceptionController.java
@@ -1,5 +1,6 @@
 package hello.exception.api;
 
+import hello.exception.exception.UserException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
@@ -16,7 +17,13 @@ public class ApiExceptionController {
         if("ex".equals(id)){
             throw new RuntimeException("잘못된 사용자입니다.");
         }
-        return new MemberDto(id, "hello member");
+        if ("bad".equals(id)) {
+            throw new IllegalArgumentException("잘못된 입력 값");
+        }
+        if ("user-ex".equals(id)) {
+            throw new UserException("사용자 오류");
+        }
+        return new MemberDto(id, "hello "+id);
     }
 
     @Data
